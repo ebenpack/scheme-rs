4c330aab63464a7869e9cb40805ba800d3b8cca9
ebenpack <ebenpackwood@gmail.com>
2023-04-30 11:19:23 -0400

    Complete chapter 4

--- scheme-rs/tests/little_schemer/4_numbers_games.test.scm
+++ scheme-rs/tests/little_schemer/4_numbers_games.test.scm
@@ -1,66 +1,36 @@
 (define (test a b) (eq? a b))
 
+(define (add1 n) (+ n 1))
+(define (sub1 n) (- n 1))
+
+(define (add m n)
+  (if (zero? m)
+    n
+    (add (sub1 m) (add1 n))))
+(define (sub m n)
+  (if (zero? n)
+    m
+    (sub (sub1 m) (sub1 n))))
+
+(define (tup+ tup1 tup2)
+  (cond
+    ((and (null? tup1) (null? tup2)) '())
+    (else (cons (+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2))))))
+
 (test (atom? 1729) #t)
 (test (number? -3) #t)
-;; (test (number? 3.14159) #t)
+(test (number? 3.14159) #t)
 (test (+ 46 12) 58)
-
-(define (occur* a l)
-  (cond
-    ((null? l) 0)
-    ((atom? (car l))
-      (cond
-        ((eq? (car l) a) (+ 1 (occur* a (cdr l))))
-        (else (occur* a (cdr l)))
-        ))
-    (else (+ (occur* a (car l)) (occur* a (cdr l))))))
-
-(test (occur* 'banana '((banana)
-    (split ((((banana ice)))
-    (cream (banana))
-    sherbet))
-    (banana)
-    (bread)
-    (banana brandy))) 5)
-
-(define (subst* new old l)
-  (cond
-    ((null? l) '())
-    ((atom? (car l))
-      (cond
-        ((eq? (car l) old) (cons new (subst* new old (cdr l))))
-        (else (cons (car l) (subst* new old (cdr l))))))
-    (else (cons (subst* new old (car l)) (subst* new old (cdr l))))))
-
-(test
-(subst* 'orange 'banana
-  '((banana)
-    (split ((((banana ice)))
-    (cream (banana))
-    sherbet))
-    (banana)
-    (bread)
-    (banana brandy)))
-  '((orange)
-    (split ((((orange ice)))
-    (cream (orange))
-    sherbet))
-    (orange)
-    (bread)
-    (orange brandy)))
-
-(define (calculate l)
-  (cond 
-    ((atom? l) l)
-    ((and (atom? car l) (atom? (cdr l))) )
-    ((list? (car l)) (calculate (car l)))
-    ((eq? (length l) 1) (car l))
-    ((eq? (car (cdr l)) '+) (+ (car l) (calculate (cddr l))))
-    ((eq? (car (cdr l)) 'x) (* (car l) (calculate (cddr l))))))
-
-
-(test (calculate '(1 + (2 x 5))) 11)
-(test (calculate '(1 + 2 x 5)) 11)
+(test (add1 67) 68)
+(test (sub1 67) 66)
+(test (zero? -0.0) #t)
+(test (zero? 0) #t)
+(test (zero? 1729) #f)
+(test (- 14 3) 11)
+(test (- 17 9) 8)
+(test (* 5 3) 15)
+(test (* 13 4) 52)
+(test (tup+ '(2 3) '(4 6)) '(6 9))
 
 ;;
 'OK
