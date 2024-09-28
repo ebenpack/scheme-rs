b7ca5a2f3e80d1d12683d52a3ee533ee0973347c
ebenpack <ebenpackwood@gmail.com>
2024-09-28 12:10:38 -0400

    Fix eq?

--- scheme-rs/src/primitive_functions/primitive_functions.rs
+++ scheme-rs/src/primitive_functions/primitive_functions.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
 use crate::environment::Bindings;
 use crate::error::{Arity, LispResult};
@@ -27,31 +28,9 @@ fn eq(args: Vec<LispVal>) -> LispResult<LispVal> {
         [LispVal::Char(arg1), LispVal::Char(arg2)] => Ok(LispVal::Bool(arg1 == arg2)),
         [LispVal::Atom(arg1), LispVal::Atom(arg2)] => Ok(LispVal::Bool(arg1 == arg2)),
         [LispVal::DottedList(xs, x), LispVal::DottedList(ys, y)] => {
-            if let LispVal::Bool(false) = eq(vec![(**x).clone(), (**y).clone()])? {
-                return Ok(LispVal::Bool(false));
-            }
-            if xs.len() != ys.len() {
-                return Ok(LispVal::Bool(false));
-            }
-            for (x, y) in xs.iter().zip(ys.iter()) {
-                if let LispVal::Bool(false) = eq(vec![x.clone(), y.clone()])? {
-                    return Ok(LispVal::Bool(false));
-                }
-            }
-            Ok(LispVal::Bool(true))
+            Ok(LispVal::Bool(Rc::ptr_eq(xs, ys) && Rc::ptr_eq(x, y)))
         }
-
-        [LispVal::List(xs), LispVal::List(ys)] => {
-            if xs.len() != ys.len() {
-                return Ok(LispVal::Bool(false));
-            }
-            for (x, y) in xs.iter().zip(ys.iter()) {
-                if let LispVal::Bool(false) = eq(vec![x.clone(), y.clone()])? {
-                    return Ok(LispVal::Bool(false));
-                }
-            }
-            Ok(LispVal::Bool(true))
-        }
+        [LispVal::List(xs), LispVal::List(ys)] => Ok(LispVal::Bool(Rc::ptr_eq(xs, ys))),
         [LispVal::Func(f), LispVal::Func(g)] => Ok(LispVal::Bool(f == g)),
         [LispVal::PrimitiveFunc(f), LispVal::PrimitiveFunc(g)] => Ok(LispVal::Bool(f == g)),
         [_, _] => Ok(LispVal::Bool(false)),
