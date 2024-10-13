abd5c2d9ec386df94b91cd51a07a153a4b3871b1
ebenpack <ebenpackwood@gmail.com>
2024-10-13 17:42:57 -0400

    New build - 2024-10-13 17:42:57

---
+++ scheme_rs_wasm_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_thing_free(a: number): void;
+export function thing_new(a: number): number;
+export function thing_eval(a: number, b: number, c: number, d: number): void;
+export function thing_read_port(a: number, b: number, c: number, d: number): void;
+export function evaluate(a: number, b: number, c: number): void;
+export function read_eval_print(a: number, b: number, c: number): void;
+export function __wbg_port_free(a: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
